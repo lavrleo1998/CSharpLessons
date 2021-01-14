13c5aafd5821b1e41219f1e574a985978790eb9d
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-01-14 17:53:36 +0300

    Перенёс проект

--- JsonDataBaseFootball/Form1.Designer.cs
+++ JsonDataBaseFootball/Form1.Designer.cs
@@ -391,13 +391,13 @@ namespace JsonDataBaseFootball
             // 
             // tabPage2
             // 
+            this.tabPage2.BackColor = System.Drawing.Color.DarkMagenta;
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(610, 317);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
