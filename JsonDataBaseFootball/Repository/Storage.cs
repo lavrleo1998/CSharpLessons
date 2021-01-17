212271686155df3a35ff724fe53810e4e82197c0
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-01-17 21:45:48 +0300

    Обновил Storage. Добавил комментарии.

--- JsonDataBaseFootball/Repository/Storage.cs
+++ JsonDataBaseFootball/Repository/Storage.cs
@@ -12,21 +12,22 @@ namespace JsonDataBaseFootball.Repository
     {
         public static void Save<T>(T objectForJSON)
         {
-            List<T> write = new List<T>();
-            //Получаем данные из файла
-            write = Get<T>();
+            //Получаем данные из файла в лист
+            List<T> write = Get<T>();
             //добавляем новый объект в лист
             write.Add(objectForJSON);
+            //Записываем обновленный лист в файл
             Write(JsonConvert.SerializeObject(write), typeof(T).Name + ".txt");
         }
-
         public static List<T> Get<T>()
         {
+            //Получаем данные из файла
             var str = Read(typeof(T).Name + ".txt");
+            //десериализация до объекта
             var result = JsonConvert.DeserializeObject<List<T>>(str);
+            //ВОзвращение объекта
             return result;
         }
-
         private static void Write(string stringToWrite, string fileAddress)
         {
             using (StreamWriter file = new StreamWriter(fileAddress, false, System.Text.Encoding.Default))
