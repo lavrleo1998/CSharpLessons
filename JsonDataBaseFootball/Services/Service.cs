5a71561b34b0e48b2e9789447b7e2ba0e4497f69
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-02-03 15:52:01 +0300

    Попытка сделать всё через get properties

---
+++ JsonDataBaseFootball/Services/Service.cs
@@ -0,0 +1,26 @@
+﻿using JsonDataBaseFootball.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonDataBaseFootball.Entities;
+namespace JsonDataBaseFootball.Services
+{
+    class Service
+    {
+        public static string Download<T>()
+
+        {
+            var d = new Event();
+            
+            string str = "События\n";
+            foreach (var evenT in Repository.Get<T>())
+            {
+
+                str += "Айди: " + 
+            }
+            return str;
+        }
+    }
+}
